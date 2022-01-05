c50ae60bd59b1c6266dd58d1035a6089a11b0129
Vasily Bulygin <vb.samara@gmail.com>
2022-01-05 10:46:18 +0400

    Added support to open CIA files

--- src/gui/ViewModels/MainWindowViewModel.cs
+++ src/gui/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,80 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using DynamicData;
+using ReactiveUI;
 
 namespace langley.gui.ViewModels
 {
+    public class FileViewModel : BaseViewModel
+    {
+        public FileViewModel(string path)
+        {
+            Path = path;
+        }
+
+        public string Path { get; }
+    }
+
     public class MainWindowViewModel : BaseViewModel
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private static readonly List<FileDialogFilter> Filters = new()
+        {
+            new FileDialogFilter
+            {
+                Extensions = new List<string> {"cia"},
+                Name = "CIA files"
+            }
+        };
+
+        private FileViewModel? _selectedFile;
+        private ReactiveCommand<Unit, Unit>? _addFileCommand;
+        private ReactiveCommand<Unit, Unit>? _removeFileCommand;
+
+        public ObservableCollection<FileViewModel> Files { get; } = new();
+
+        public FileViewModel? SelectedFile
+        {
+            get => _selectedFile;
+            set => this.RaiseAndSetIfChanged(ref _selectedFile, value);
+        }
+
+        public Interaction<List<FileDialogFilter>, string[]?> ShowFileOpenDialog { get; } = new();
+
+        #region Commands
+
+        #region AddFileCommand
+
+        public ReactiveCommand<Unit, Unit> AddFileCommand => _addFileCommand ??= ReactiveCommand.CreateFromTask(AddFileAsync);
+
+        private async Task AddFileAsync()
+        {
+            var files = await ShowFileOpenDialog.Handle(Filters);
+            if(files is null)
+                return;
+            Files.AddRange(files.Select(x=> new FileViewModel(x)));
+        }
+
+        #endregion
+
+        #region RemoveFileCommand
+
+        public ReactiveCommand<Unit, Unit> RemoveFileCommand =>
+            _removeFileCommand ??= ReactiveCommand.Create(RemoveFile);
+
+        private void RemoveFile()
+        {
+            if(_selectedFile is null)
+                return;
+            Files.Remove(_selectedFile);
+        }
+
+        #endregion
+
+        #endregion
     }
 }
--- src/gui/Views/MainWindow.axaml.cs
+++ src/gui/Views/MainWindow.axaml.cs
@@ -1,10 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using langley.gui.ViewModels;
+using ReactiveUI;
+#pragma warning disable 8602
 
 namespace langley.gui.Views
 {
-    public partial class MainWindow : Window
+    public class MainWindow : ReactiveWindow<MainWindowViewModel>
     {
         public MainWindow()
         {
@@ -12,6 +18,17 @@ namespace langley.gui.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
+            this.WhenActivated(action => action(ViewModel.ShowFileOpenDialog.RegisterHandler(ShowOpenFileDialogAsync)));
+        }
+
+        private async Task ShowOpenFileDialogAsync(InteractionContext<List<FileDialogFilter>, string[]?> interaction)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filters = interaction.Input
+            };
+            var files = await dialog.ShowAsync(this);
+            interaction.SetOutput(files);
         }
 
         private void InitializeComponent()
